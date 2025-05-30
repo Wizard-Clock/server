4a9d8689fc3678aab2a91d513547b374e5105be0
Andrew Feely <91284672+AndrewFeelz@users.noreply.github.com>
2025-05-30 15:59:46 -0500

    add usage of sqlite

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+import sqlite from 'sqlite3';
+const app = express();
+const db = new sqlite.Database(":memory:"); //remembrall.db
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
