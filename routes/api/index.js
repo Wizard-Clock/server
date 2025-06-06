87da19ceaa06488f7c982d12e9b362a000bedd55
Andrew Feely <91284672+AndrewFeelz@users.noreply.github.com>
2025-06-06 15:34:44 -0500

    initial api implement

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,10 +3,22 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 const db = require("../../handlers/dbHandler");
 const authenticateToken = require("../../handlers/authHandler");
+const passport = require("passport");
 
-router.get('/', authenticateToken, async function (req, res, next) {
-    const role = await db.getRoleNameFromUserID(req.user.id);
-    res.render('index', {title: 'Home', username: req.user.username, role: role[0].role});
+router.post('/login', async function (req, res, next) {
+    passport.authenticate('local', (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+        req.login(user, {session: false}, err => {
+            if (err) {
+                return res.status(400).json({ message: 'Invalid credentials' });
+            }
+            const token = jwt.sign(user.id, process.env.JWT_SECRET);
+            return res.json({ token })
+        })
+    })
+    (req, res);
 });
 
 module.exports = router;
