3cc0faf73019956786b1a61ee6f62ec1a43e4d8f
Andrew Feely <91284672+AndrewFeelz@users.noreply.github.com>
2025-06-16 09:58:14 -0500

    cors added.

--- app.js
+++ app.js
@@ -19,7 +19,13 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
+const cors=require("cors");
+const corsOptions ={
+  origin:'*',
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+app.use(cors(corsOptions))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
