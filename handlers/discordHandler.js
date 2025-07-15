a365b38cc30104c2672527d141656756cf956455
Andrew Feely <91284672+AndrewFeelz@users.noreply.github.com>
2025-07-15 11:33:43 -0500

    removed reliance on random npm discord pkg

--- handlers/discordHandler.js
+++ handlers/discordHandler.js
@@ -1,20 +1,61 @@
-const { Webhook, MessageBuilder} = require('discord-webhook-node');
-const discordHook = new Webhook(process.env.DISCORD_WEBHOOK_URL);
+const webhookUsername = "Dobby";
+const webhookAvatar = "https://static.wikia.nocookie.net/harrypotter/images/f/f0/Dobbyelve.jpg";
+let startup = true;
+notifyServerStartup();
 
-discordHook.setUsername('Dobby');
-discordHook.setAvatar('https://static.wikia.nocookie.net/harrypotter/images/f/f0/Dobbyelve.jpg');
-
-const startupDate = new Date(Date.now());
-discordHook.send("Server has started up at " + startupDate.toUTCString());
+async function notifyServerStartup() {
+    let params = {
+        username: webhookUsername,
+        avatar_url: webhookAvatar,
+        content: "Server has started up at " + new Date().toUTCString()
+    }
+    await sendWebhook(params);
+}
 
 async function notifyLocationChange(username, clockPositionName) {
-    const messageBuilder = new MessageBuilder()
-        .setTitle('Wizard has Moved!')
-        .addField("Wizard", "Master " + username, true)
-        .addField("", "", true)
-        .addField("Location", clockPositionName, true)
-        .setTimestamp();
-    discordHook.send(messageBuilder);
+    let params = {
+        username: webhookUsername,
+        avatar_url: webhookAvatar,
+        embeds: [
+            {
+                "title": "Wizard has Moved!",
+                "fields": [
+                    {
+                        "name": "Wizard",
+                        "value": "Master " + username,
+                        "inline": true
+                    },
+                    {
+                        "name": "Location",
+                        "value": clockPositionName,
+                        "inline": true
+                    }
+                ]
+            }
+        ],
+        content: new Date().toUTCString()
+    }
+    sendWebhook(params);
+}
+
+async function sendWebhook(params) {
+    fetch(process.env.DISCORD_WEBHOOK_URL, {
+        method: "POST",
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(params)
+    }).then(res => {
+        console.log(res);
+        if (startup) {
+            if (res.status === 200) {
+                console.log("Discord Webhooks Enabled");
+            } else if (res.status === 401) {
+                console.log("Invalid Discord Webhook URL. Discord Webhooks Disabled.");
+            }
+        }
+        startup = false;
+    });
 }
 
 module.exports = {
