e7b703cfd5cd2b705cd247bdbaf82bd389b02909
Andrew Feely <91284672+AndrewFeelz@users.noreply.github.com>
2025-07-28 16:39:34 -0500

    finished sever setting functionality

--- routes/smt/serverSettings.js
+++ routes/smt/serverSettings.js
@@ -2,19 +2,16 @@ const express = require('express');
 const router = express.Router();
 const authenticateToken = require("../../handlers/authHandler");
 const db = require("../../handlers/dbHandler");
-const serverSettingsService = require("../../handlers/serverSettingHandler");
-const {formidable} = require("formidable");
-const settingsService = serverSettingsService.default.getInstance();
+const dobby = require("../../handlers/discordHandler");
+const settingsService = require("../../handlers/serverSettingHandler").default.getInstance();
 
 
 router.get('/', authenticateToken, async function (req, res, next) {
-
     const userRole = await db.getRoleFromUserID(req.userID);
     if (userRole.role !== "admin") {
         return res.redirect("/clock");
     }
     const user = await db.getUserFromID(req.userID);
-    const sdf = settingsService.getAllSettings();
 
     res.render('serverSettings', {
         title: 'Server Settings',
@@ -24,11 +21,21 @@ router.get('/', authenticateToken, async function (req, res, next) {
     });
 });
 
-router.post('/updateSettings', authenticateToken, async function (req, res, next) {
-    const form = formidable({ multiples: true });
-    await form.parse(req, async (err, settings) => {
-        console.log(settings);
-    });
+router.post('/updateDiscordSettings', authenticateToken, async function (req, res, next) {
+    let discordSettings = [
+        { name: "discordWebhook", value: req.body.discordWebhook},
+        { name: "enableDiscord", value: req.body.enableDiscord},
+        { name: "notifyEveryPositionUpdate", value: req.body.notifyEveryPositionUpdate},
+    ];
+
+    for (let discordSetting of discordSettings) {
+        await settingsService.set(discordSetting.name, discordSetting.value);
+    }
+
+    if (req.body.enableDiscord) {
+        dobby.enableDiscordPlugin();
+    }
+    res.send({success: true});
 })
 
 module.exports = router;
